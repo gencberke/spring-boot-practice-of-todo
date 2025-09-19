6e48cea3213647ab668f3504ff789fdb97198284
gencberke <berkegenc.dev@gmail.com>
2025-09-19 15:15:25 +0300

    created project with database, application yml, exc... configurations

---
+++ src/main/java/com/berkedev/practicespringboottodoapp/PracticeSpringBootTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.berkedev.practicespringboottodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringBootTodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringBootTodoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/berkedev/practicespringboottodoapp/PracticeSpringBootTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkedev.practicespringboottodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringBootTodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
