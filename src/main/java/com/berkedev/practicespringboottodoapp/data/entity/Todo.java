28606cff07bf8aa167af75894625bcbd307764fc
gencberke <berkegenc.dev@gmail.com>
2025-09-23 17:30:31 +0300

    enhance Todo entity with additional fields and builder pattern

--- src/main/java/com/berkedev/practicespringboottodoapp/data/entity/Todo.java
+++ src/main/java/com/berkedev/practicespringboottodoapp/data/entity/Todo.java
@@ -1,13 +1,19 @@
 package com.berkedev.practicespringboottodoapp.data.entity;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.Generated;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "todo")
 @Data
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class Todo {
 
@@ -16,4 +22,91 @@ public class Todo {
     @SequenceGenerator(name = "todo_seq", sequenceName = "todo_id_seq", allocationSize = 1)
     private Long id;
 
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private boolean completed = false;
+
+    private LocalDateTime dueAt;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Nullable
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+
+        if (updatedAt == null)
+            updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    private Todo(Builder builder) {
+        this.title = builder.title;
+        this.description = builder.description;
+        this.completed = builder.completed;
+        this.dueAt = builder.dueAt;
+        this.createdAt = builder.createdAt;
+        this.updatedAt = builder.updatedAt;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String title;
+        private String description;
+        private boolean completed = false;
+        private LocalDateTime dueAt;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder setCompleted(boolean completed) {
+            this.completed = completed;
+            return this;
+        }
+
+        public Builder setDueAt(LocalDateTime dueAt) {
+            this.dueAt = dueAt;
+            return this;
+        }
+
+        public Builder setCreatedAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public Builder setUpdatedAt(LocalDateTime updatedAt) {
+            this.updatedAt = updatedAt;
+            return this;
+        }
+
+        public Builder withCurrentTimestamp() {
+            this.createdAt = LocalDateTime.now();
+            return this;
+        }
+
+        public Todo build() {return new Todo(this);}
+    }
 }
